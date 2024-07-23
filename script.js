5f62abff0f3e3dbb6945cd8dda80a61998c26b91
Morell <walter.morell38@gmail.com>
2024-07-23 23:10:52 +0200

    dd

--- script.js
+++ script.js
@@ -12,18 +12,69 @@ const quand votre variable est voué à rester constante ( exemple : const pi = 
 
 //  stocker de l'information
 
-function displayAge() {
+//function displayAge() {
 
 // 1. Récuperation de l'information
 
-let age = document.getElementById("age").value;
+//let age = document.getElementById("age").value;
 
 // 2. Traitement
 
-const textAge = "Vous avez " + age + " ans";  /** ou const textAge = `Vous avez ${age} ans`;	**/
+//const textAge = "Vous avez " + age + " ans";  /** ou const textAge = `Vous avez ${age} ans`;	**/
 
 
 //3. Afficher l'information
 
-document.getElementById("textAge").innerHTML = textAge;
-}
+//document.getElementById("textAge").innerHTML = textAge;
+//}
+
+
+/**
+ * Array Methods
+ * 
+ * tostring()
+ * join()
+ * pop()
+ * push()
+ * shift()
+ * unshift()
+ * 
+ */
+
+
+//to string
+//array.toString() : convertit un tableau en une chaîne de caractères et renvoie cette chaîne de caractères.
+//let languages = ["HTML", "CSS", "JavaScript", "PHP"];
+//console.log(languages.toString());
+
+//join()
+//array.join(separator) : convertit un tableau en une chaîne de caractères et renvoie cette chaîne de caractères.
+//let languages = ["HTML", "CSS", "JavaScript", "PHP"];
+//plusieurs séparateurs
+//console.log(languages.join(" - "));
+//console.log(languages.join(""));
+//console.log(languages.join(", "));
+//console.log(languages.join(" | "));
+//console.log(languages.join(" / "));
+
+//pop()
+//array.pop() : supprime le dernier élément d'un tableau et renvoie cet élément.
+//let languages = ["HTML", "CSS", "JavaScript", "PHP"];
+//console.log(languages.pop());
+//console.log(languages);
+//push()
+
+//array.push() : ajoute un ou plusieurs éléments à la fin d'un tableau et renvoie la nouvelle longueur du tableau.
+let languages = ["HTML", "CSS", "JavaScript", "PHP"];
+console.log(languages.push("Python"));
+
+//shift()
+//array.shift() : supprime le premier élément d'un tableau et renvoie cet élément.
+//let languages = ["HTML", "CSS", "JavaScript", "PHP"];
+//console.log(languages.shift());
+
+//unshift()
+//array.unshift() : ajoute un ou plusieurs éléments au début d'un tableau et renvoie la nouvelle longueur du tableau.
+//let languages = ["HTML", "CSS", "JavaScript", "PHP"];
+//console.log(languages.unshift("Python"));
+
